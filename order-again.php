e27d3454e4b6aea5a63cd4485f021bfbd3536e48
Islam <ilyasogluislam@gmail.com>
2023-02-12 11:26:49 +0300

    Create order-again.php

---
+++ order-again.php
@@ -0,0 +1,15 @@
+<?php
+/**
+	 * Order Again @ My Account Orders
+	 */
+	add_filter( 'woocommerce_my_account_my_orders_actions', 'tlc_order_again_action', 50, 2 );
+
+	function tlc_order_again_action( $actions, $order ) {
+		if ( $order->has_status( 'completed' ) ) {
+			$actions['order-again'] = array(
+				'url' => wp_nonce_url( add_query_arg( 'order_again', $order->get_id(), wc_get_cart_url() ), 'woocommerce-order_again' ),
+				'name' => __( 'Order again', 'woocommerce' ),
+			);
+		}
+		return $actions;
+	}
