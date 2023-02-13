b5d95e54bfc64187777220e14f2f4a27064645b4
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2023-02-13 10:28:52 +0300

    Create woocommerce-breadcrumb.php

---
+++ woocommerce-breadcrumb.php
@@ -0,0 +1,17 @@
+
+// WooCommerce Breadcrumb
+if (!function_exists('bs_woocommerce_breadcrumbs')) :
+  add_filter('woocommerce_breadcrumb_defaults', 'bs_woocommerce_breadcrumbs');
+  function bs_woocommerce_breadcrumbs() {
+    return array(
+      'delimiter'   => '',
+      'wrap_before' => "<nav aria-label='breadcrumb' class='wc-breadcrumb breadcrumb-scroller mb-4 mt-2 py-2 px-3 bg-light rounded'>
+      <ol class='breadcrumb mb-0'>",
+      'wrap_after'  => '</ol>
+      </nav>',
+      'before'      => '<li class="breadcrumb-item">',
+      'after'       => '</li>',
+      'home'        => _x('Home', 'breadcrumb', 'woocommerce'),
+    );
+  }
+endif;
