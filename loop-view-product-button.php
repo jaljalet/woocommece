1db529d765b0d3b69906791688b63afdbd5e0185
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2023-02-20 21:14:41 +0300

    Create loop-view-product-button.php

---
+++ loop-view-product-button.php
@@ -0,0 +1,5 @@
+function your_theme_woocommerce_loop_add_to_cart_link( $html, $product ) {
+	return '<a href="' . get_permalink( $product->id ) . '" class="button">' . __( 'View Product' ) . '</a>';
+}
+
+add_filter( 'woocommerce_loop_add_to_cart_link', 'your_theme_woocommerce_loop_add_to_cart_link', 10,2 );
