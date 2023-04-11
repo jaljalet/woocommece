e2d915d08d96d379442bc3173e137edb2a4f0279
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2023-04-11 11:32:31 +0300

    Update order-again.php

--- order-again.php
+++ order-again.php
@@ -2,9 +2,9 @@
 /**
 	 * Order Again @ My Account Orders
 	 */
-	add_filter( 'woocommerce_my_account_my_orders_actions', 'your_theme_order_again_action', 50, 2 );
+	add_filter( 'woocommerce_my_account_my_orders_actions', 'yourdomain_order_again_action', 50, 2 );
 
-	function your_theme_order_again_action( $actions, $order ) {
+	function yourdomain_order_again_action( $actions, $order ) {
 		if ( $order->has_status( 'completed' ) ) {
 			$actions['order-again'] = array(
 				'url' => wp_nonce_url( add_query_arg( 'order_again', $order->get_id(), wc_get_cart_url() ), 'woocommerce-order_again' ),
