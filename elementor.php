eade85b4b73b0ca7e2774d77cddb2a52b5e0c90e
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2023-06-22 11:07:38 +0300

    Update elementor.php

--- elementor.php
+++ elementor.php
@@ -5,3 +5,6 @@ add_action( 'elementor/query/children_filter', function( $query ) {
 	// Modify the query
 	$query->set( 'post_parent', $current_pageID );
 });
+
+// Disable elementor google fonts
+add_filter( 'elementor/frontend/print_google_fonts', '__return_false' );
