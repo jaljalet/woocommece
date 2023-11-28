9f7e8e6b40be3af37ec286200a950fcaf65ad4c3
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2023-11-28 10:57:46 +0300

    Delete elementor-cat-post-list-filter.php

--- elementor-cat-post-list-filter.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Showing latest post in post same category Posts Widget
-add_action( 'elementor/query/children_cat_filter', function( $query ) {
-	$current_pageID = get_queried_object();
-	$posts_my = get_postdata($current_pageID);
-	print_r();
-	// Modify the query
-	$query->set( 'cat', $posts_my['Category'][0] );
-});
