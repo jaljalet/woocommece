d46798e67edd4a4ac37cf7e024d6f2fbb1698c30
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2024-07-24 14:44:48 +0300

    Update check-if-logged-in-user-has-already-purchased-a-product.php

--- check-if-logged-in-user-has-already-purchased-a-product.php
+++ check-if-logged-in-user-has-already-purchased-a-product.php
@@ -1,1 +1,11 @@
+<?php
+add_action( 'woocommerce_after_shop_loop_item', 'theme_user_logged_in_product_already_bought', 30 );
+  
+function theme_user_logged_in_product_already_bought() {
+   global $product;
+   if ( ! is_user_logged_in() ) return;
+   if ( wc_customer_bought_product( '', get_current_user_id(), $product->get_id() ) ) {
+      echo '<div>You purchased this in the past. Buy again?</div>';
+   }
+}
 
