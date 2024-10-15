de97e4ca67e52720dabc93e95796e61049a68b5f
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2024-10-15 11:43:52 +0300

    Create add-first-last-name-to-my-account-register-form.php

---
+++ add-first-last-name-to-my-account-register-form.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @snippet Add First & Last Name to My Account Register Form - WooCommerce
+ */
+  
+///////////////////////////////
+// 1. ADD FIELDS
+  
+add_action( 'woocommerce_register_form_start', 'yourdomain_add_name_woo_account_registration' );
+  
+function yourdomain_add_name_woo_account_registration() {
+    ?>
+  
+    <p class="form-row form-row-first">
+    <label for="reg_billing_first_name"><?php _e( 'First name', 'woocommerce' ); ?> <span class="required">*</span></label>
+    <input type="text" class="input-text" name="billing_first_name" id="reg_billing_first_name" value="<?php if ( ! empty( $_POST['billing_first_name'] ) ) esc_attr_e( $_POST['billing_first_name'] ); ?>" />
+    </p>
+  
+    <p class="form-row form-row-last">
+    <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label>
+    <input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?>" />
+    </p>
+  
+    <div class="clear"></div>
+  
+    <?php
+}
+  
+///////////////////////////////
+// 2. VALIDATE FIELDS
+  
+add_filter( 'woocommerce_registration_errors', 'yourdomain_validate_name_fields', 10, 3 );
+  
+function yourdomain_validate_name_fields( $errors, $username, $email ) {
+    if ( isset( $_POST['billing_first_name'] ) && empty( $_POST['billing_first_name'] ) ) {
+        $errors->add( 'billing_first_name_error', __( '<strong>Error</strong>: First name is required!', 'woocommerce' ) );
+    }
+    if ( isset( $_POST['billing_last_name'] ) && empty( $_POST['billing_last_name'] ) ) {
+        $errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required!.', 'woocommerce' ) );
+    }
+    return $errors;
+}
+  
+///////////////////////////////
+// 3. SAVE FIELDS
+  
+add_action( 'woocommerce_created_customer', 'yourdomain_save_name_fields' );
+  
+function yourdomain_save_name_fields( $customer_id ) {
+    if ( isset( $_POST['billing_first_name'] ) ) {
+        update_user_meta( $customer_id, 'billing_first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
+        update_user_meta( $customer_id, 'first_name', sanitize_text_field($_POST['billing_first_name']) );
+    }
+    if ( isset( $_POST['billing_last_name'] ) ) {
+        update_user_meta( $customer_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
+        update_user_meta( $customer_id, 'last_name', sanitize_text_field($_POST['billing_last_name']) );
+    }
+  
+}
