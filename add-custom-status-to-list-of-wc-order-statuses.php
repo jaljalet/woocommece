320c8752e4555e6d140e0b44b3edaa7043de1eb1
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2024-12-13 10:41:50 +0300

    Update add-custom-status-to-list-of-wc-order-statuses.php

--- add-custom-status-to-list-of-wc-order-statuses.php
+++ add-custom-status-to-list-of-wc-order-statuses.php
@@ -1,13 +1,22 @@
 <?php
-function yourdomain_add_custom_to_order_statuses( $order_statuses ) {
-   $new_order_statuses = array();
-   // add new order status after processing
-   foreach ( $order_statuses as $key => $status ) {
-      $new_order_statuses[ $key ] = $status;
-      if ( 'wc-processing' === $key ) {
-         $new_order_statuses['wc-transfer'] = 'Transfer';
-      }
-   }
-   return $new_order_statuses;
+add_filter( 'woocommerce_register_shop_order_post_statuses', 'yourdomain_register_custom_order_status' );
+ 
+function yourdomain_register_custom_order_status( $order_statuses ) {
+   // Status must start with "wc-"!
+   $order_statuses['wc-custom-status'] = array(
+      'label' => 'Custom Status',
+      'public' => false,
+      'exclude_from_search' => false,
+      'show_in_admin_all_list' => true,
+      'show_in_admin_status_list' => true,
+      'label_count' => _n_noop( 'Custom Status <span class="count">(%s)</span>', 'Custom Status <span class="count">(%s)</span>', 'woocommerce' ),
+);
+   return $order_statuses;
 }
-add_filter( 'wc_order_statuses', 'yourdomain_add_custom_to_order_statuses' );
+ 
+add_filter( 'wc_order_statuses', 'yourdomain_show_custom_order_status_single_order_dropdown' );
+ 
+function yourdomain_show_custom_order_status_single_order_dropdown( $order_statuses ) {
+   $order_statuses['wc-custom-status'] = 'Custom Status';
+   return $order_statuses;
+}
