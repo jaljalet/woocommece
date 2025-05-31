63eb45c2d3ce3bcb1f8f4e6531d078b83f97f74f
Islam Ilyasoglu <ilyasogluislam@gmail.com>
2025-05-31 16:06:06 +0300

    Update sql-command-delete-actionscheduler.php

--- sql-command-delete-actionscheduler.php
+++ sql-command-delete-actionscheduler.php
@@ -3,3 +3,6 @@ DELETE FROM wp_actionscheduler_actions WHERE status = 'complete' LIMIT 1000;
 
 -- Delete 1000 logs at a time
 DELETE FROM wp_actionscheduler_actions WHERE status = 'failed' LIMIT 1000;
+
+-- Delete 1000 logs at a time
+DELETE FROM wp_actionscheduler_logs WHERE action_id NOT IN ( SELECT action_id FROM wp_actionscheduler_actions ) LIMIT 1000;
